b476c35f55330fe7a68ae7492f4cbce817dd84ef
alpertunga-bile <kingdom19online@hotmail.com>
2023-04-23 12:27:53 +0300

    added new features

---
+++ GUI.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+from Parser import Parser
+import os
+
+class GUI:
+    window = None
+    startFrame = None
+    initializeFrame = None
+    parser = Parser()
+
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry("600x120")
+
+        tempPath = os.path.join(os.getcwd(), "test.md")
+
+        self.startFrame = tk.Frame()
+
+        directoryLabel = tk.Label(master=self.startFrame, text=f"{tempPath}")
+        directoryLabel.grid(column=0, row=0, padx=(0, 50), ipady=5)
+
+        selectButton = tk.Button(
+            master=self.startFrame,
+            text="Select Prompt Files",
+            command=self.parser.SelectDirectory
+        )
+
+        selectButton.grid(column=1, row=0)
+
+        self.startFrame.pack()
+
+        self.parser.directoryLabel = directoryLabel
+
+        initializeFrame = tk.Frame()
+
+        initializeButton = tk.Button(
+            master=initializeFrame,
+            text="Parse And Save",
+            command=self.parser.Run
+        )
+
+        informationLabel = tk.Label(master=self.initializeFrame, text="")
+
+        initializeButton.pack()
+        informationLabel.pack(pady=10)
+        initializeFrame.pack()
+
+        self.parser.textLabel = informationLabel
+    
+    def Loop(self):
+        self.window.mainloop()
---
+++ Parser.py
@@ -0,0 +1,81 @@
+from tkinter import filedialog
+from tkinter import messagebox
+import os
+
+class Parser:
+    promptFiles = None
+    textLabel = None
+    directoryLabel = None
+
+    def SelectDirectory(self):
+        self.promptFiles = filedialog.askopenfilenames(initialdir=os.getcwd())
+        while len(self.promptFiles) == 0:
+            messagebox.showerror(message="You dont select any markdown file! Please select at least one file to proceed")
+            self.promptFiles = filedialog.askopenfilenames(initialdir=os.getcwd())
+        text = "1 file is selected" if len(self.promptFiles) == 1 else f"{len(self.promptFiles)} files are selected"
+        self.directoryLabel.config(text=text)
+
+    def Run(self):       
+        for promptFile in self.promptFiles:
+            text = f"Parsing {promptFile} ..."
+            self.textLabel.config(text=text)
+            self.ParseAndSave(promptFile)
+            text = f"{text}   DONE!!!"
+            self.textLabel.config(text=text)
+        
+        text = "1 file is Parsed!!! Happy Prompting!!!" if len(self.promptFiles) == 1 else f"{len(self.promptFiles)} files are Parsed!!! Happy Prompting!!!"
+        self.textLabel.config(text=text)
+
+    def ParseAndSave(self, promptFile):
+        file = open(promptFile, "r")
+        lines = file.readlines()
+        file.close()
+
+        filename = os.path.splitext(promptFile)[0]
+        
+        positiveFilename = f"{filename}_positive.txt"
+        negativeFilename = f"{filename}_negative.txt"
+
+        positiveStr = ""
+        negativeStr = ""
+
+        isPositive = True
+        for line in lines:
+            if line.find("Positive Prompts") != -1:
+                isPositive = True
+            elif line.find("Negative Prompts") != -1:
+                isPositive = False
+            
+            if line.startswith("#") or line == "\n" or line.startswith("---") or line == '  \n':
+                continue
+
+            if line.startswith("- [ ] ") or line.startswith("- [x] "):
+                line = line[6:]
+            elif line.startswith("-[ ] ") or line.startswith("-[x] "):
+                line = line[5:]
+            elif line.startswith("-[] "):
+                line = line[4:]
+            elif line.startswith("- ") or line.startswith("> "):
+                line = line[2:]
+                
+            if line.endswith("\n"):
+                line = line[:-1]
+
+            line = line + ","
+
+            if isPositive:
+                positiveStr = positiveStr + line
+            else:
+                negativeStr = negativeStr + line
+
+        positiveStr = positiveStr[:-1]
+        negativeStr = negativeStr[:-1]
+
+        positiveFile = open(positiveFilename, "w")
+        negativeFile = open(negativeFilename, "w")
+
+        positiveFile.write(positiveStr)
+        negativeFile.write(negativeStr)
+
+        positiveFile.close()
+        negativeFile.close()
