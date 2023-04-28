eea5307593635d062da26bd267f89a9a9263312c
alpertunga-bile <76731692+alpertunga-bile@users.noreply.github.com>
2023-04-28 08:02:13 +0000

    Fixed some bugs

--- GUI.py
+++ GUI.py
@@ -10,10 +10,14 @@ class GUI:
     window = None
     startFrame = None
     initializeFrame = None
-    debugLabel = None
     parser = Parser()
 
     def __init__(self):
+        """
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        // Initialize Window
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        """
         ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("green")
 
@@ -21,14 +25,15 @@ class GUI:
         self.window.title("Markdown Prompt Parser")
         self.window.geometry("600x400")
 
+        """
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        // Change Appearance
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        """
+
         iconImage = ctk.CTkImage(light_image=Image.open("icons/light_icon.png"),
                                 dark_image=Image.open("icons/dark_icon.png"),
                                 size=(30, 30))
-
-        """
-        self.debugLabel = ctk.CTkLabel(self.window, text="")
-        self.debugLabel.pack()
-        """
 
         appearanceChangeButton = ctk.CTkButton(
             self.window,
@@ -41,6 +46,26 @@ class GUI:
         )
         appearanceChangeButton.pack(side=ctk.TOP, anchor=ctk.NW)
 
+        """
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        // Translation Variables
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        """
+        self.parser.translatorName = ctk.StringVar(value='Google')
+        translatorCombobox = ctk.CTkComboBox(
+            master=self.window,
+            width=150,
+            variable=self.parser.translatorName,
+            state='readonly',
+            values=[
+                'Google',
+                'Mymemory',
+                'Linguee',
+                'PONS'
+            ]
+        )
+        translatorCombobox.pack()
+
         self.parser.translateValue = tk.IntVar()
         translateCheckButton = ctk.CTkCheckBox(
             master=self.window,
@@ -51,6 +76,12 @@ class GUI:
         )
         translateCheckButton.pack(pady=20)
 
+        """
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        // Parse All Files
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        """
+
         tempPath = os.path.join(os.getcwd(), "prompts")
 
         fastButton = ctk.CTkButton(
@@ -58,12 +89,17 @@ class GUI:
             text="Parse All Files In 'prompts' Folder",
             command=self.ParseAllFiles
         )
-        fastButton.pack(pady=30)
+        fastButton.pack(pady=10)
 
+        """
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        // Select Markdown Files
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        """
         self.startFrame = ctk.CTkFrame(master=self.window)
 
         directoryLabel = ctk.CTkLabel(master=self.startFrame, text=f"{tempPath}")
-        directoryLabel.grid(column=0, row=0, padx=(0, 50), ipady=5)
+        directoryLabel.grid(column=0, row=0, padx=(0, 50), ipady=5, pady=10)
 
         selectButton = ctk.CTkButton(
             master=self.startFrame,
@@ -73,8 +109,13 @@ class GUI:
         selectButton.grid(column=1, row=0)
 
         self.startFrame.pack()
-
         self.parser.directoryLabel = directoryLabel
+
+        """
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        // Parse And Save Selected Files
+        //////////////////////////////////////////////////////////////////////////////////////////////
+        """
 
         initializeFrame = ctk.CTkFrame(master=self.window)
 
@@ -101,18 +142,30 @@ class GUI:
         self.parser.textLabel = informationLabel
         self.parser.progressBar = progressBar
     
+    """
+    Refresh The GUI Application
+    """
     def Refresh(self):
         self.window.update()
         self.window.after(1000, self.Refresh)
 
+    """
+    Run Parse And Save function in new thread
+    """
     def Parse(self):
         self.Refresh()
         threading.Thread(target=self.parser.Run).start()
 
+    """
+    Run ParseAllFiles function in new thread
+    """
     def ParseAllFiles(self):
         self.Refresh()
         threading.Thread(target=self.parser.ParseAllFiles).start()
 
+    """
+    Change Appearance based on the button click
+    """
     def ChangeAppearance(self):
         if ctk.get_appearance_mode() == "Dark":
             ctk.set_appearance_mode("light")
--- Parser.py
+++ Parser.py
@@ -1,20 +1,31 @@
 from tkinter import filedialog
 import os
 from glob import glob
-from deep_translator import GoogleTranslator
+from deep_translator import GoogleTranslator, MyMemoryTranslator, LingueeTranslator, PonsTranslator
 
 class Parser:
     promptFiles = None
     textLabel = None
     directoryLabel = None
+    
     translateValue = None
-    progressBar = None
+    translator = None
+    translatorName = None
 
+    progressBar = None
+    isTranslate = False
+
+    """
+    Select Markdown Files
+    """
     def SelectDirectory(self):
         self.promptFiles = filedialog.askopenfilenames(initialdir=os.getcwd())
         text = "1 file is selected" if len(self.promptFiles) == 1 else f"{len(self.promptFiles)} files are selected"
         self.directoryLabel.configure(text=text)
 
+    """
+    Parse All Files in 'prompts' folder
+    """
     def ParseAllFiles(self):
         fastPath = os.path.join(os.getcwd(), "prompts")
         if(os.path.exists(fastPath) == False):
@@ -24,25 +35,64 @@ class Parser:
         self.textLabel.configure(text=f"{self.promptFiles.count} files are found ...")
         self.Run()
 
+    """
+    Select translator if 'Translate Prompts' checkbox is checked
+    """
+    def SelectTranslator(self):
+        if self.isTranslate == False:
+            return
+        
+        translatorName = self.translatorName.get()
+        if translatorName == 'Google':
+            self.translator = GoogleTranslator(source='auto', target='en')
+        elif translatorName == 'Mymemory':
+            self.translator = MyMemoryTranslator(source='auto', target='en')
+        elif translatorName == 'Linguee':
+            self.translator = LingueeTranslator(source='auto', target='english')
+        elif translatorName == "PONS":
+            self.translator = PonsTranslator(source='auto', target='en')
+
+    """
+    Run the parsing
+    """
     def Run(self):
+        """
+        Check if there are no files selected
+        """
         if self.promptFiles is None:
             self.textLabel.configure(text=f"Please Select At Least One File")
             return
+        
+        """
+        Set progressbar variables
+        """
         self.progressBar.set(0)
         self.progressBar.pack(ipadx=100, pady=10)
         parsedFileCount = 0
         totalPromptFiles = len(self.promptFiles)
+        
+        """
+        Choose translator if it is going to be used
+        """
+        self.isTranslate = True if self.translateValue.get() else False
+        self.SelectTranslator()
+        
         for promptFile in self.promptFiles:
             if(os.path.exists(promptFile) == False):
                 self.textLabel.configure(text=f"{promptFile} is not exists | Skipping ...")
                 continue
+            
             text = f"Parsing {promptFile}"
             self.textLabel.configure(text=text)
             self.ParseAndSave(promptFile)
             text = f"{text}   DONE!!!"
+            self.textLabel.configure(text=text)
+
+            """
+            Update progressbar for every iteration
+            """
             parsedFileCount = parsedFileCount + 1
             self.progressBar.set(float(parsedFileCount) / float(totalPromptFiles))
-            self.textLabel.configure(text=text)
         
         if parsedFileCount == 1:
             text = "1 file is Parsed!!! Happy Prompting!!!"
@@ -53,22 +103,51 @@ class Parser:
 
         self.textLabel.configure(text=text)
         self.progressBar.pack_forget()
-
+    
+    """
+    Preprocess the line that get from markdown file
+    """
     def Preprocess(self, line):
+        """
+        Check mostly used starting syntax
+        """
         if line.startswith("- [ ] ") or line.startswith("- [x] "):
-                line = line[6:]
+            line = line[6:]
         elif line.startswith("-[ ] ") or line.startswith("-[x] "):
             line = line[5:]
         elif line.startswith("-[] "):
             line = line[4:]
         elif line.startswith("- ") or line.startswith("> "):
             line = line[2:]
-            
+        
+        """
+        Check for newline operator
+        """
         if line.endswith("\n"):
             line = line[:-1]
 
-        if self.translateValue.get():
-            line = GoogleTranslator(source='auto', target='en').translate(line)
+        """
+        Check for whitespaces
+        """
+        line = line.strip()
+
+        """
+        translation progress is done line by line because summed string's length will exceed the character limit of translator
+        """
+        if self.isTranslate:
+            if self.translatorName.get() == 'Google':
+                line = self.translator.translate(line)
+            else:
+                tempStrArray = line.split(',')
+                tempStr = ""
+                for temp in tempStrArray:
+                    tempStr += f"{self.translator.translate(temp)}, "
+                return tempStr
+
+        """
+        Add comma to seperate prompts and provide continuousness 
+        """
+        line = line + ", "
 
         return line
 
@@ -77,6 +156,9 @@ class Parser:
         lines = file.readlines()
         file.close()
 
+        """
+        Get folder path where is the Markdown file located
+        """
         filename = os.path.splitext(promptFile)[0]
         
         positiveFilename = f"{filename}_positive.txt"
@@ -87,31 +169,37 @@ class Parser:
 
         isPositive = True
         for line in lines:
+            """
+            Determine if positive or negative prompt
+            """
             if line.find("Positive Prompts") != -1:
                 isPositive = True
             elif line.find("Negative Prompts") != -1:
                 isPositive = False
             
+            """
+            Continue if it is heading, new line, long line
+            """
             if line.startswith("#") or line == "\n" or line.startswith("---") or line == '  \n':
                 continue
 
             line = self.Preprocess(line)
-
-            line = line + ", "
 
             if isPositive:
                 positiveStr = positiveStr + line
             else:
                 negativeStr = negativeStr + line
 
+        """
+        Delete comma and white ', ' space which is added in preprocess stage from the last of the line
+        """
         positiveStr = positiveStr[:-2]
         negativeStr = negativeStr[:-2]
 
         positiveFile = open(positiveFilename, "w")
-        negativeFile = open(negativeFilename, "w")
-
         positiveFile.write(positiveStr)
-        negativeFile.write(negativeStr)
-
         positiveFile.close()
+
+        negativeFile = open(negativeFilename, "w")
+        negativeFile.write(negativeStr)
         negativeFile.close()
